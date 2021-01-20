3a39f96ed05e574f26875c0de2b5d76edd6d493e
AltTabber <mikeice414@gmail.com>
2021-01-21 00:47:29 +0300

    Добавил темы + адаптер для получения сообщений

--- src/main/java/com/alttabber/malbot/resources/discord/MALDiscordListenerAdapter.java
+++ src/main/java/com/alttabber/malbot/resources/discord/MALDiscordListenerAdapter.java
@@ -1,5 +1,6 @@
 package com.alttabber.malbot.resources.discord;
 
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -12,5 +13,16 @@ public class MALDiscordListenerAdapter extends ListenerAdapter {
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         System.out.println("received message = " + event.getAuthor().getName());
         System.out.println(event.getMessage().getContentDisplay());
+
+        if(event.getMessage().getContentRaw().startsWith("!nc")){
+            String command = event.getMessage().getContentRaw().substring(3).trim();
+            checkRandomDrawingTheme(command, event);
+        }
+    }
+
+    private void checkRandomDrawingTheme(String command, MessageReceivedEvent event) {
+        if(command.startsWith("randomTheme")){
+            event.getChannel().sendMessage("Случайную тему выбрал " + event.getMember().getAsMention()).queue();
+        }
     }
 }
